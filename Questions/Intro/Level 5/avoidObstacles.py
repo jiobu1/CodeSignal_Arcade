39985074a0d2c3a7a1faf93832154ce759a15f6f
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-30 10:37:09 -0500

    avoidObstacles.py

--- Questions/Intro/Level 5/avoidObstacles.py
+++ Questions/Intro/Level 5/avoidObstacles.py
@@ -44,6 +44,35 @@ def avoidObstacles(inputArray):
 
     return options[0]
 
+def avoidObstacles(inputArray):
+
+    # sort the list in ascending order
+    obs = sorted(inputArray)
+
+    # set jump distance to 1
+    jump_dist = 1
+
+    # flag to check if current jump distance
+    # hits an obstacle
+    obstacle_hit = True
+
+    while(obstacle_hit):
+
+        obstacle_hit = False
+        jump_dist += 1
+
+        # checking if jumping with current length
+        # hits an obstacle
+        for i in range(0, len(obs)):
+            if obs[i] % jump_dist == 0:
+
+                # if obstacle is hit repeat process
+                # after increasing jump distance
+                obstacle_hit = True
+                break
+
+    return jump_dist
+
 # inputArray =  [5, 3, 6, 7, 9] # 4
 # inputArray =  [2, 3] # 4
 # inputArray =  [1, 4, 10, 6, 2] # 7
